0429193ceca8835e358150a545bbdd04a159a141
ShaimaNouri-308 <66574499+ShaimaNouri-308@users.noreply.github.com>
2020-10-07 20:25:50 +0300

    st7

---
+++ speedtest-7/speedtest-7/DetailVC.swift
@@ -0,0 +1,34 @@
+//
+//  DetailVC.swift
+//  speedtest-7
+//
+//  Created by Shaima Nouri on 10/7/20.
+//  Copyright © 2020 kuwaitcodes.cohort2. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    @IBOutlet weak var nameField: UILabel!
+    var name: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nameField.text = name?.description 
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- speedtest-7/speedtest-7/ViewController.swift
+++ speedtest-7/speedtest-7/ViewController.swift
@@ -10,12 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    var name: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    @IBAction func next(_ sender: Any) {
+        let name = nameField.text
+        self.name = name
+        if nameField.text!.count >= 2 {
+            performSegue(withIdentifier: "details", sender: name)
+        }
+        else{
+            print ("WHAT'S YOUR NAME")
+        }
+    }
     // Hint: `performSegue` is the way
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let name = sender as! String
+        let detailsVC = segue.destination as! DetailVC
+        detailsVC.name = self.name
+    }
 
 }
 
